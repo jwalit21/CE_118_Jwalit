1fff5805b96d453fe4ceed00f6c31fad229493d4
jwalit21 <jwalishah2q@gmail.com>
2020-09-16 09:58:14 +0530

    Lab8 dotnet core MVC initial commit

---
+++ Lab8/Task1-Command_line_Hello_world/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyConsoleApp_Lab81
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World !! Hello Folks !!");
+        }
+    }
+}
---
+++ Lab8/Task2-4_MVC/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Lab8_Task4_BasicMVC.Models;
+
+namespace Lab8_Task4_BasicMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Test1()
+        {
+            int num1 = 56,num2 = 8;
+            int sum = num1 + num2;
+            ViewBag.num1 = num1.ToString();
+            ViewBag.num2 = num2.ToString();
+            ViewBag.sum = sum.ToString();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Test2()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Test2(string num1,string num2)
+        {
+            string sum = (int.Parse(num2) + int.Parse(num1)).ToString();
+            ViewBag.num1 = num1;
+            ViewBag.num2 = num2;
+            ViewBag.sum = sum;
+            return View() ;
+        }
+
+        [HttpGet]
+        public IActionResult User_Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult User_Registration(string num1, string num2)
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
