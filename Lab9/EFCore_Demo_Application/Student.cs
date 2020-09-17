c78cc77349854ad482a296c62f944fb73128500f
jwalit21 <jwalishah2q@gmail.com>
2020-09-17 19:03:54 +0530

    Lab9 EFCore MVC initial commit

---
+++ Lab9/EFCore_Demo_Application/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace demo_EFCore.Models
+{
+    public class Student
+    {
+        public int ID { set; get; }
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+        public float cpi { set; get; }
+        public string email { set; get; }
+        public string mobile { set; get; }
+    }
+}
